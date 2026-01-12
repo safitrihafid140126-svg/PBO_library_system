40e7c73528aac264819230c54ab410ca756536a7
Safitri Hafid_24107 <safitrihafid140126@gmail.com>
2026-01-12 19:03:43 +0900

    Add Buku class with display method

--- buku.py
+++ buku.py
@@ -1,1 +1,12 @@
 
+class Buku:
+    def __init__(self, judul, penulis):
+        self.judul = judul
+        self.penulis = penulis
+
+    def tampilkan(self):
+        print(f"judul: {self.judul}")
+        print(f"Penulis: {self .penulis}")
+
+buku1 = Buku("Python Dasar", "Safitri")
+buku1 .tampilkan()
